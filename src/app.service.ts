3e59dd88cc5191ebf82ca5ae93b76de23a3d7d91
ahhammadi <ah_hammadi@hotmail.com>
2021-04-11 15:36:31 +0200

    intial

---
+++ src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import * as os from 'os';
+
+@Injectable()
+export class AppService {
+  getHostName(): string {
+    return `Host Name is ${os.hostname()}.`;
+  }
+}
