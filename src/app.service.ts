8eebf198943428e750b43ef105d247e63f963861
Ahmed Hammadi <ahmed.hammadi.ext@aptar.com>
2021-06-10 01:38:44 +0200

    read app version from package jason file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import * as os from 'os';
+var pjson = require('./package.json');
 
 @Injectable()
 export class AppService {
   getHostName(): string {
-    return `Host Name is ${os.hostname()} for v (1.6.0).`;
+    return `Host Name is ${os.hostname()} for v (${pjson.version}).`;
   }
 }
