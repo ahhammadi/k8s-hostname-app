20ab6bf3cbf2497b1692e4f7bf5960dde7c985ae
Ahmed Hammadi <ahmed.hammadi.ext@aptar.com>
2021-06-10 04:45:48 +0200

    import package.json file to get version in app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import * as os from 'os';
-var pjson = require('./package.json');
+import * as packageJson from '../package.json';
 
 @Injectable()
 export class AppService {
   getHostName(): string {
-    return `Host Name is ${os.hostname()} for v (${pjson.version}).`;
+    return `Host Name is ${os.hostname()} for v (${packageJson.version}).`;
   }
 }
